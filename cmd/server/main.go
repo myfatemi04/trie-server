80105a45273cc6b9e31c83e119a0c7ccb826d9b0
Michael Fatemi <myfatemi04@gmail.com>
2021-08-27 18:57:38 -0400

    add go server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/google/logger"
+	"github.com/gorilla/websocket"
+	"github.com/myfatemi04/trie/pkg/trie"
+)
+
+var port = flag.Uint("port", 3333, "port to listen on")
+
+func main() {
+	flag.Parse()
+
+	if *port > 65535 {
+		logger.Fatalf("port %d is invalid", port)
+	}
+
+	server := trie.NewServer()
+
+	upgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			logger.Errorf("error upgrading connection: %v", err)
+			return
+		}
+
+		server.HandleClient(conn)
+	})
+
+	addr := fmt.Sprintf(":%d", *port)
+	http.ListenAndServe(addr, nil)
+}
